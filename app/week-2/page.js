1c30590c78a8e6e3b1f4157e3493331f3f0ffbde
baselchc <baselchono@gmail.com>
2024-05-27 18:44:44 -0600

    Initial commit

---
+++ app/week-2/page.js
@@ -0,0 +1,14 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+    return (
+      <main>
+        <h1>Assignment 1, Week 2</h1>
+        <br></br>
+        <h1>Shopping List</h1>
+        <br></br>
+        <StudentInfo />
+      </main>
+    );
+  }
+  
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Basel Chono</h1>
+      <Link href="https://github.com/baselchc/cprg306-assignments.git">
+        Click here to go to my GitHub Repository!
+      </Link>
+    </div>
+  );
+};
+
+export default StudentInfo;
