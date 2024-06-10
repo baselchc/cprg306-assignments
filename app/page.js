199520f20fc7bb161486a535e9f2a284b0020696
baselchc <baselchono@gmail.com>
2024-06-10 08:20:32 -0600

    Assignment 4
    A4 WebDev

--- app/page.js
+++ app/page.js
@@ -60,6 +60,23 @@ export default function Home() {
           Find in-depth information about assignment 3 for CPRG 306: Web Development 2.
           </p>
         </a>
+
+        <a
+          href="/week-4"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+          Assignment 4{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          Find in-depth information about assignment 4 for CPRG 306: Web Development 2.
+          </p>
+        </a>
       </div>
     </main>
   );
